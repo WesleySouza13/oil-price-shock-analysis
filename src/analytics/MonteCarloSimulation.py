5d11c16a2f0c4c42e32118af1c158b743983cf7a
WesleySouza13 <souzabusiness13@gmail.com>
2026-05-12 16:36:24 -0300

    fazendo n simulaçoes em diferentes cenarios

--- src/analytics/MonteCarloSimulation.py
+++ src/analytics/MonteCarloSimulation.py
@@ -6,14 +6,12 @@ class MonteCarlo():
     def __init__(self, data:pd.DataFrame):
         self.data = data
 
-    def normal_scenario(self, n_simulations:int) -> pd.DataFrame:
-        np.random.seed(42)
+    def normal_scenario(self,  steps=int) -> pd.DataFrame:
         mc_list = []
         for num in self.data.columns:
             mu = self.data[num].mean()
             std = self.data[num].std()
-            
-            mc = np.random.normal(mu, std, n_simulations)
+            mc = np.random.normal(mu, std, steps)
             df = pd.Series(mc, name=num)
             mc_list.append(df)
             
@@ -21,7 +19,6 @@ class MonteCarlo():
     
     def sim_scenario(self, n_simulations:int, shock:float, col_shock:str):
         """"use o shock sabendo que sera em %. Ex: 2% -> 2, 3% -> 3..."""
-        np.random.seed(42)
         mc_list = []
         for num in self.data.columns:
             if col_shock in num:
