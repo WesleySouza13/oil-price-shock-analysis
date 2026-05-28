f86db8b1bd96cb6cf0ddab5c902d4492b29836c5
WesleySouza13 <souzabusiness13@gmail.com>
2026-05-28 02:52:21 -0300

    feat: criando pagina para forecast

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import os
 
 timeseries_path = os.path.join('app', 'time_series_page.py')
 simulation_path = os.path.join('app', 'simulation.py')
+prev_path = os.path.join('app', 'forecast.py')
 
-time_series = st.Page(timeseries_path, title='Time Series')
-simulation = st.Page(simulation_path, title='Simulation')
-
-pg = st.navigation([time_series, simulation])
+time_series = st.Page(timeseries_path, title='Time Series', icon='📈')
+simulation = st.Page(simulation_path, title='Simulation', icon='⚙️')
+forecast = st.Page(prev_path, title='Forecast', icon='🔮')
+pg = st.navigation([time_series, simulation, forecast])
 pg.run()
